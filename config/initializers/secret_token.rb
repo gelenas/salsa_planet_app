7f94099dc9816eb90012288230fad68ae1820450
GS <gelenas@hotmail.com>
2013-11-15 15:06:25 -0800

    Initialize directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalsaPlanetApp::Application.config.secret_key_base = '06356cfb3b539bae560e2a2399b8b42e22668b6dab45b434569dbdf2ddde6f27fe534adc1cfdf7ac31efef16ea5a936094240d23d6abfb5f2a5e88d9dbdfafff'
